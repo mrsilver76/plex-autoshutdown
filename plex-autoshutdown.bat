ebc0166f1a6ea7de61f7936ceebf25ee9b415980
mrsilver76 <5719686+mrsilver76@users.noreply.github.com>
2026-03-14 17:04:03 +0530

    Add files via upload

--- plex-autoshutdown.bat
+++ plex-autoshutdown.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 rem Plex Autoshutdown (for Windows)
-rem Version 1.2 (released 28th March 2025)
+rem Version 1.3 (released 14th March 2025)
 rem https://github.com/mrsilver76/plex-autoshutdown
 rem
 rem A simple script which, when executed, will check that no-one is using Plex
@@ -34,7 +34,10 @@ rem WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 rem
 rem For more information, please refer to https://unlicense.org
 
-rem ----- Configuration settings -------------------------------------------
+rem ----- Basic configuration settings -----------------------------------
+rem
+rem Only one setting (your PLEX_TOKEN) is required to get this script running.
+rem You can leave everything else as-is and the script will work perfectly fine.
 
 rem PLEX_TOKEN
 rem The API token required for this script to be able to access Plex.
@@ -42,6 +45,11 @@ rem Do not share your token with anyone. For details on how to find this, see
 rem https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
 
 set PLEX_TOKEN=abcd1234efgh5678
+
+rem ----- Advanced configuration settings --------------------------------
+rem
+rem You do not have to change any of these settings. The script works fine
+rem with the defaults. These options are only for fine-tuning behaviour.
 
 rem MIN_UPTIME
 rem The minimum amount of time (in seconds) the server needs to be running
@@ -51,63 +59,202 @@ rem for that period of time. The recommended value is 7200 = 2 hours.
 
 set MIN_UPTIME=7200
 
-rem ----- End of configuration settings. Code starts here ------------------
+rem BLOCKING_PROCESSES
+rem A semi-colon (;) separated list of processes that will block shutdown
+rem if they are running. This is useful for delaying shutdown until certain
+rem tasks (either Plex related or not) finish. For example we include
+rem "Plex Transcoder.exe" here to ensure that the server isn’t incorrectly
+rem shut down while Plex is transcoding - even if the Plex API reports no
+rem activity.
 
-rem Get the uptime
+set BLOCKING_PROCESSES=Plex Transcoder.exe
 
-Set uptime_seconds=
+rem BLOCKING_ADDRESSES
+rem A semi-colon (;) separated list of devices that will block shutdown if
+rem they are active. Each entry can be an IP address or hostname
+rem (e.g. "192.168.0.20;SAMSUNGTV") and devices on this list are assumed to
+rem be in use if they respond to a network ping. If you want to use
+rem IP addresses then it is recommended to configure your router to
+rem assign a static (same) IP address to the device to stop it changing.
 
-(for /F "tokens=1,* delims==" %%v in (
-    'wmic path Win32_PerfFormattedData_PerfOS_System get SystemUptime /format:list ^| findstr "[0-9]"'
-) do (
-    Set uptime_seconds=%%w
-))
+set BLOCKING_ADDRESSES=
+
+rem ----- End of configuration settings. Code starts here ----------------
+
+set VERSION=1.3
+set DO_SHUTDOWN=true
+
+rem Parse arguments looking for test mode defined
+
+set "TEST_MODE=false"
+for %%A in (%*) do (
+	if "%%A"=="-t" set "TEST_MODE=true"
+	if "%%A"=="--test" set "TEST_MODE=true"
+	if "%%A"=="/t" set "TEST_MODE=true"
+	if "%%A"=="/test" set "TEST_MODE=true"
+)
+
+rem Display starting up message
+
+if "%TEST_MODE%"=="true" (
+	call :Log Plex Autoshutdown v%VERSION% starting ^(test mode^)...
+) else (
+	call :Log Plex Autoshutdown v%VERSION% starting...
+)
+
+rem Check if the Plex token has been set correctly
+
+if "%PLEX_TOKEN%"=="" (
+    call :Log Error: Plex token is not set
+    exit /b 1
+) else if "%PLEX_TOKEN%"=="abcd1234efgh5678" (
+    call :Log Error: Plex token is set to the default placeholder
+    exit /b 1
+)
+
+rem Check if Plex is running and contactable
+
+curl -s -o "%TEMP%\plex-autoshutdown.tmp" -w "%%{http_code}" --connect-timeout 3 "http://127.0.0.1:32400/status/sessions?X-Plex-Token=%PLEX_TOKEN%" > "%TEMP%\plex_http_code.tmp"
+set /p HTTP_CODE=<"%TEMP%\plex_http_code.tmp"
+del "%TEMP%\plex_http_code.tmp"
+
+rem Plex not running or unreachable
+
+if "%HTTP_CODE%"=="000" (
+    call :Log Error: Plex is not running or not reachable
+    del /f "%TEMP%\plex-autoshutdown.tmp"
+    exit /b 1
+)
+
+REM Plex running but the token is invalid
+
+if "%HTTP_CODE%"=="401" (
+    call :Log Error: Plex is running but the token is invalid
+    del /f "%TEMP%\plex-autoshutdown.tmp"
+    exit /b 1
+)
+
+rem Plex responded with a HTTP code that wasn't 200 (success)
+
+if not "%HTTP_CODE%"=="200" (
+    call :Log Error: Plex responded with HTTP %HTTP_CODE%
+    del /f "%TEMP%\plex-autoshutdown.tmp"
+    exit /b 1
+)
+
+rem Something responded, but it doesn't look like Plex
+
+findstr /c:"<MediaContainer" "%TEMP%\plex-autoshutdown.tmp" >nul
+if errorlevel 1 (
+    call :Log Error: Response does not appear to be Plex
+    del /f "%TEMP%\plex-autoshutdown.tmp"
+    exit /b 1
+)
+
+rem Get the uptime and check it's not less than the configured amount
+
+set uptime_seconds=
+
+for /f %%i in ('powershell.exe -NoProfile -NonInteractive -Command "[int]((New-TimeSpan -Start (Get-CimInstance Win32_OperatingSystem).LastBootUpTime -End (Get-Date)).TotalSeconds)"') do (
+    set uptime_seconds=%%i
+)
 
 if %uptime_seconds% LSS %MIN_UPTIME% (
-	echo Script terminated. Uptime is %uptime_seconds% seconds, which is less than %MIN_UPTIME%.
-	exit /b
+	call :Log Uptime is %uptime_seconds% seconds, which is less than %MIN_UPTIME%
+    set DO_SHUTDOWN=false
 )
 
 rem Check if Plex has any active streams
 
-curl -s "http://127.0.0.1:32400/status/sessions?X-Plex-Token=%PLEX_TOKEN%" | find /I "MediaContainer size=""0"">" >NUL
+curl -s "http://127.0.0.1:32400/status/sessions?X-Plex-Token=%PLEX_TOKEN%" -o "%TEMP%\plex-autoshutdown.tmp" >NUL
+find /I "MediaContainer size=""0"">" "%TEMP%\plex-autoshutdown.tmp" >NUL
 if %errorlevel%==1 (
-	echo Script terminated. Plex is streaming.
-	exit /b
+    call :Log Plex is streaming content
+	set DO_SHUTDOWN=false
 )
 
 rem Check if Plex is downloading
 
 curl -s "http://127.0.0.1:32400/activities?X-Plex-Token=%PLEX_TOKEN%" -o "%TEMP%\plex-autoshutdown.tmp" >NUL
-
 find /I "type=""media.download""" "%TEMP%\plex-autoshutdown.tmp" >NUL
 if %errorlevel%==0 (
-    echo Script terminated. Plex is downloading.
-	del /f "%TEMP%\plex-autoshutdown.tmp"
-	exit /b
+    call :Log Plex is downloading content
+	set DO_SHUTDOWN=false
 ) 
 
 rem Check if Plex is transcoding
 
 find /I "type=""media.offline.transcode""" "%TEMP%\plex-autoshutdown.tmp" >NUL
 if %errorlevel%==0 (
-	echo Script terminated. Plex is transcoding.
-	del /f "%TEMP%\plex-autoshutdown.tmp"
-	exit /b
+	call :Log Plex is transcoding content
+	set DO_SHUTDOWN=false
 )
 
 rem Check if Plex is streaming or recording live TV
 
 find /I "type=""grabber.grab""" "%TEMP%\plex-autoshutdown.tmp" >NUL
 if %errorlevel%==0 (
-	echo Script terminated. Plex is streaming or recording live TV.
-	del /f "%TEMP%\plex-autoshutdown.tmp"
-	exit /b
+	call :Log Plex is streaming or recording live TV
+	set DO_SHUTDOWN=false
 )
+
+rem Clean up
 
 del /f "%TEMP%\plex-autoshutdown.tmp"
 
+rem Check if any processes are running that would block shutdown
+
+if defined BLOCKING_PROCESSES (
+    for %%P in (%BLOCKING_PROCESSES%) do (
+        set PROC_NAME=%%P
+        set PROC_NAME=!PROC_NAME:;=!
+        tasklist /FI "IMAGENAME eq !PROC_NAME!" 2>NUL | find /I "!PROC_NAME!" >NUL
+        if !errorlevel!==0 (
+            call :Log Found process running: !PROC_NAME!
+            set DO_SHUTDOWN=false
+        )
+    )
+)
+
+rem Check if any local addresses are in use that would block shutdown
+rem
+rem Note: Early versions also checked the ARP cache, but dynamic entries can
+rem remain in both Windows and Linux for hours after a device powers off.
+rem In testing, a TV turned off 5 hours earlier was still listed in the ARP
+rem cache, making this method unreliable for detecting active devices.
+
+if defined BLOCKING_ADDRESSES (
+    for %%T in (%BLOCKING_ADDRESSES%) do (
+ 
+        rem Ping the address/hostname to see if device is online
+
+        ping -n 1 -w 2000 %%T | find "TTL=" >NUL
+        if !errorlevel! EQU 0 (
+            call :Log Found active device at %%T
+            set DO_SHUTDOWN=false
+        )
+    )
+)
+
 rem Shut down the server
 
-echo Shutting down now...
-shutdown /s /f
+if "%DO_SHUTDOWN%"=="false" (
+    call :Log Autoshutdown finished ^(shutdown blocked^)
+    exit /b 1
+)
+
+if "%TEST_MODE%"=="true" (
+	call :Log Shutdown would happen now ^(blocked by test mode^)
+) else (
+	call :Log Shutting down now...
+	shutdown /s /t 0
+)
+exit /b 0
+
+rem Log
+rem Echos text to stdout prefixed with the date and time, ideal for
+rem sending to a text log for monitoring and debugging purposes
+:Log
+for /f "delims=" %%I in ('powershell -NoProfile -NonInteractive -Command "Get-Date -Format \"yyyy-MM-dd HH:mm:ss\" "') do set "TS=%%I"
+echo [%TS%] %*
+goto :eof
--- plex-autoshutdown.sh
+++ plex-autoshutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Plex Autoshutdown (for Linux)
-# Version 1.2 (released 28th March 2025)
+# Version 1.3 (released 14th March 2025)
 # https://github.com/mrsilver76/plex-autoshutdown
 #
 # A simple script which, when executed, will check that no-one is using Plex
@@ -33,14 +33,22 @@
 #
 # For more information, please refer to https://unlicense.org
 
-# ----- Configuration settings -------------------------------------------
+# ----- Basic configuration settings -----------------------------------
+#
+# Only one setting (your PLEX_TOKEN) is required to get this script running.
+# You can leave everything else as-is and the script will work perfectly fine.
 
 # PLEX_TOKEN
 # The API token required for this script to be able to access Plex.
 # Do not share your token with anyone. For details on how to find this, see
 # https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
 
-PLEX_TOKEN=abcd1234efgh5678
+PLEX_TOKEN="abcd1234efgh5678"
+
+# ----- Advanced configuration settings --------------------------------
+#
+# You do not have to change any of these settings. The script works fine
+# with the defaults. These options are only for fine-tuning behaviour.
 
 # MIN_UPTIME
 # The minimum amount of time (in seconds) the server needs to be running
@@ -50,57 +58,200 @@ PLEX_TOKEN=abcd1234efgh5678
 
 MIN_UPTIME=7200
 
-# ----- End of configuration settings. Code starts here ------------------
+# BLOCKING_PROCESSES
+# A semi-colon (;) separated list of processes that will block shutdown
+# if they are running. This is useful for delaying shutdown until certain
+# tasks (either Plex related or not) finish. For example we include
+# "Plex Transcoder" here to ensure that the server isn’t incorrectly
+# shut down while Plex is transcoding - even if the Plex API reports no
+# activity.
 
-readonly MIN_UPTIME
-readonly PLEX_TOKEN
+BLOCKING_PROCESSES="Plex Transcoder"
 
-# Get the uptime in seconds
+# BLOCKING_ADDRESSES
+# A semi-colon (;) separated list of devices that will block shutdown if
+# they are active. Each entry can be an IP address or hostname
+# (e.g. "192.168.0.20;SAMSUNGTV") and devices on this list are assumed to
+# be in use if they respond to a network ping. If you want to use
+# IP addresses then it is recommended to configure your router to
+# assign a static (same) IP address to the device to stop it changing.
+
+BLOCKING_ADDRESSES=""
+
+# ----- End of configuration settings. Code starts here ----------------
+
+VERSION="1.3"
+DO_SHUTDOWN=true
+
+# Log
+# Echos text to stdout prefixed with the date and time, ideal for
+# sending to a text log for monitoring and debugging purposes
+Log() {
+    TS=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "[$TS] $*"
+}
+
+# Parse arguments looking for test mode defined
+
+TEST_MODE=false
+for arg in "$@"; do
+	lower_arg="${arg,,}"
+	case "$lower_arg" in
+		-t|--test|/t|/test)
+			TEST_MODE=true
+			;;
+	esac
+done
+
+# Display starting up message
+
+if [ "$TEST_MODE" = true ]; then
+    Log "Plex Autoshutdown v$VERSION starting (test mode)..."
+else
+    Log "Plex Autoshutdown v$VERSION starting..."
+fi
+
+# Check if the Plex token has been set correctly
+
+if [ -z "$PLEX_TOKEN" ]; then
+    Log "Error: Plex token is not set"
+    exit 1
+elif [ "$PLEX_TOKEN" = "abcd1234efgh5678" ]; then
+    Log "Error: Plex token is set to the default placeholder"
+    exit 1
+fi
+
+# Check if Plex is running and contactable
+
+rm -f "/tmp/plex-autoshutdown.tmp"
+HTTP_CODE=$(curl -s -o "/tmp/plex-autoshutdown.tmp" -w "%{http_code}" --connect-timeout 3 "http://127.0.0.1:32400/status/sessions?X-Plex-Token=$PLEX_TOKEN")
+
+# Plex not running or unreachable
+
+if [ "$HTTP_CODE" = "000" ]; then
+    Log "Error: Plex is not running or not reachable"
+    rm -f "/tmp/plex-autoshutdown.tmp"
+    exit 1
+fi
+
+# Plex running but the token is invalid
+
+if [ "$HTTP_CODE" = "401" ]; then
+    Log "Error: Plex is running but the token is invalid"
+    rm -f "/tmp/plex-autoshutdown.tmp"
+    exit 1
+fi
+
+# Plex responded with a HTTP code that wasn't 200 (success)
+
+if [ "$HTTP_CODE" != "200" ]; then
+    Log "Error: Plex responded with HTTP $HTTP_CODE"
+    rm -f "/tmp/plex-autoshutdown.tmp"
+    exit 1
+fi
+
+# Something responded, but it doesn't look like Plex
+
+if ! grep -q "<MediaContainer" "/tmp/plex-autoshutdown.tmp"; then
+    Log "Error: Response does not appear to be Plex"
+    rm -f "/tmp/plex-autoshutdown.tmp"
+    exit 1
+fi
+
+# Get the uptime and check it's not less than the configured amount
 
 uptime_seconds=$(awk '{print int($1)}' /proc/uptime)
 
-if ((uptime_seconds < MIN_UPTIME)); then
-	echo Script terminated. Uptime is $uptime_seconds seconds, which is less than $MIN_UPTIME.
-	exit 1
+if (( uptime_seconds < MIN_UPTIME )); then
+    Log "Uptime is $uptime_seconds seconds, which is less than $MIN_UPTIME"
+    DO_SHUTDOWN=false
 fi
 
 # Check if Plex has any active streams
 
-if ! curl -s "http://127.0.0.1:32400/status/sessions?X-Plex-Token=$PLEX_TOKEN" | grep -i "MediaContainer size=\"0\">" >/dev/null; then
-	echo Script terminated. Plex is streaming.
-	exit 1
+if ! grep -q 'MediaContainer size="0">' "/tmp/plex-autoshutdown.tmp"; then
+    Log "Plex is streaming content"
+    DO_SHUTDOWN=false
 fi
 
 # Check if Plex is downloading
 
-curl -s "http://127.0.0.1:32400/activities?X-Plex-Token=$PLEX_TOKEN" > /tmp/plex-autoshutdown.tmp
-
-if grep -qE 'type="media\.download"' /tmp/plex-autoshutdown.tmp; then
-	echo Script terminated. Plex is downloading.
-	rm -f /tmp/plex-autoshutdown.tmp
-	exit 1
+curl -s "http://127.0.0.1:32400/activities?X-Plex-Token=$PLEX_TOKEN" -o "/tmp/plex-autoshutdown.tmp"
+if grep -q 'type="media.download"' "/tmp/plex-autoshutdown.tmp"; then
+    Log "Plex is downloading content"
+    DO_SHUTDOWN=false
 fi
 
 # Check if Plex is transcoding
 
-if grep -qE 'type="media\.offline\.transcode"' /tmp/plex-autoshutdown.tmp; then
-	echo Script terminated. Plex is transcoding.
-	rm -f /tmp/plex-autoshutdown.tmp
-	exit 1
+if grep -q 'type="media.offline.transcode"' "/tmp/plex-autoshutdown.tmp"; then
+    Log "Plex is transcoding content"
+    DO_SHUTDOWN=false
 fi
 
 # Check if Plex is streaming or recording live TV
 
-if grep -qE 'type="grabber\.grab"' /tmp/plex-autoshutdown.tmp; then
-	echo Script terminated. Plex is streaming or recording live TV.
-	rm -f /tmp/plex-autoshutdown.tmp
-	exit 1
+if grep -q 'type="grabber.grab"' "/tmp/plex-autoshutdown.tmp"; then
+    Log "Plex is streaming or recording live TV"
+    DO_SHUTDOWN=false
 fi
 
-rm -f /tmp/plex-autoshutdown.tmp
+# Clean up
+
+rm -f "/tmp/plex-autoshutdown.tmp"
+
+# Check if any processes are running that would block shutdown
+
+if [[ -n "$BLOCKING_PROCESSES" ]]; then
+    IFS=';' read -ra PROC_ARRAY <<< "$BLOCKING_PROCESSES"
+    for PROC_NAME in "${PROC_ARRAY[@]}"; do
+
+        # Skip empty strings
+        [[ -z "$PROC_NAME" ]] && continue
+
+        if ps -ef | grep -i "$PROC_NAME" | grep -v grep >/dev/null; then
+            Log "Found process running: $PROC_NAME"
+            DO_SHUTDOWN=false
+        fi
+    done
+fi
+
+# Check if any local addresses are in use that would block shutdown
+#
+# Note: Early versions also checked the ARP cache, but dynamic entries can
+# remain in both Windows and Linux for hours after a device powers off.
+# In testing, a TV turned off 5 hours earlier was still listed in the ARP
+# cache, making this method unreliable for detecting active devices.
+
+# Only run if BLOCKING_ADDRESSES is defined
+if [[ -n "$BLOCKING_ADDRESSES" ]]; then
+    IFS=';' read -ra ADDR_ARRAY <<< "$BLOCKING_ADDRESSES"
+
+    # Loop over each address/hostname
+    for TARGET in "${ADDR_ARRAY[@]}"; do
+
+        # Ping the address/hostname to see if device is online
+
+        if ping -c 1 -W 2 "$TARGET" &>/dev/null; then
+            Log "Found active device at $TARGET"
+            DO_SHUTDOWN=false
+        fi
+
+    done
+fi
 
 # Shut down the server
 
-echo Shutting down now...
-sudo shutdown -h now
+if [ $DO_SHUTDOWN = false ]; then
+    Log "Autoshutdown finished (shutdown blocked)"
+    exit 1
+fi
+
+if [ "$TEST_MODE" = true ]; then
+    Log "Shutdown would happen now (blocked by test mode)"
+else
+    Log "Shutting down now..."
+    sudo shutdown -h now
+fi
+
 exit 0
